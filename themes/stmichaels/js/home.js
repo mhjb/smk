4383f30f72c223e14bd7982256b313b113e34690
Richard Bartlett <richarddbartlett@gmail.com>
2012-03-01 10:32:35 +1300

    workin on home page fx

--- themes/stmichaels/js/home.js
+++ themes/stmichaels/js/home.js
@@ -1,13 +1,22 @@
 $(document).ready(function() {
 
+$('#valueswrapper')
+	.mouseover(function(){
+		alert('in');
+	})
+	.mouseout(function(){
+		alert('out');
+	});
 $('#values span	')
 	.mouseover(function() {
 		$('#values-blurb').html($(this).attr('title'));
 		$(this).attr('title','');
+		$('#titlecontent').css("visibility", "hidden");		
 	})
 	.mouseout(function() {
 		$(this).attr('title', $('#values-blurb').html());
 		$('#values-blurb').html("");
+		$('#titlecontent').css("visibility", "visible");
 	});
 	
 })
