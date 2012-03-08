f310751f3238a7f6d68913579e616bbaa2b66332
Richard Bartlett <richarddbartlett@gmail.com>
2012-03-08 19:42:23 +1300

    automated Sermons and MiKel page

--- mysite/code/LatestSubPage.php
+++ mysite/code/LatestSubPage.php
@@ -7,6 +7,19 @@ class LatestSubPage extends SiteTree {
 	public static $has_one = array(
 	);
 
+	function FileList() {
+	
+		return DataObject::get( 
+		  $name = 'File', 
+		  $filter = "ClassName = 'file' and Filename like 'assets/pdfs/$this->URLSegment/%'", 
+		  $sort = "Created DESC", 
+		  $join = "", 
+		  $limit = "" 
+	   ); 
+	}
+
+	
+	
 }
 class LatestSubPage_Controller extends ContentController {
 	
